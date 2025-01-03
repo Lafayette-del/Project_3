b3ed7f689910949b04964e4450c3bf7a300b1bec
Mytisha <mytishalafayette@yahoo.com>
2025-01-02 17:09:25 -0800

    update navbar

--- src/Component/NavbarMenu/NavbarMenu.jsx
+++ src/Component/NavbarMenu/NavbarMenu.jsx
@@ -12,8 +12,8 @@ export default function NavbarMenu() {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
+          <Nav.Link href="#home">Todolist</Nav.Link>
+          <Nav.Link href="#link">Contact Page</Nav.Link>
           <NavDropdown title="Dropdown" id="basic-nav-dropdown">
             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
             <NavDropdown.Item href="#action/3.2">
